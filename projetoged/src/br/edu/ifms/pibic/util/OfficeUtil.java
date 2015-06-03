fb412d1ea8dc764c16e3c161e4325cc9fcc84d54
MariliaNeves <marilianeves68@gmail.com>
2015-06-03 17:00:36 -0400

    atualização da classe OfficeUtil.java, agora ela extra .doc .docx .xls e .xlsx

--- projetoged/src/br/edu/ifms/pibic/util/OfficeUtil.java
+++ projetoged/src/br/edu/ifms/pibic/util/OfficeUtil.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 
 import org.apache.poi.hssf.extractor.ExcelExtractor;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.xssf.extractor.XSSFExcelExtractor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
@@ -32,21 +38,66 @@ public class OfficeUtil {
 	}
 	
 	/**
+	 * Método para extrair texto doc
+	 * @param caminho
+	 * @return
+	 */
+	public static String pegaTextoWordDOC(String caminho){
+
+		POIFSFileSystem pOIFSFileSystem=null;		
+		try {
+			pOIFSFileSystem = new POIFSFileSystem(new FileInputStream(caminho));
+			HWPFDocument doc=new HWPFDocument(pOIFSFileSystem);
+			WordExtractor we=new WordExtractor(doc);			
+			return we.getText();
+			
+		}catch (  Exception e) {
+		    e.printStackTrace();
+		}
+		return "";
+	}
+	
+	/**
 	 *  Método para extrair texto xls
 	 * @param caminho
 	 * @return
 	 * @throws IOException
 	 */
-	public static String pegaTextoWordXLS(String caminho) throws IOException{
-		 
-		FileInputStream in=new FileInputStream(caminho);
-		  HSSFWorkbook wb=new HSSFWorkbook(in);
-		  ExcelExtractor extractor=new ExcelExtractor(wb);
-		  extractor.setFormulasNotResults(true);
-		  extractor.setIncludeSheetNames(true);		  
-		  String text=extractor.getText();
-		  return text;
+	public static String pegaTextoExcelXLS(String caminho) {
+		
+		try{
+			FileInputStream in=new FileInputStream(caminho);
+			  HSSFWorkbook wb=new HSSFWorkbook(in);
+			  ExcelExtractor extractor=new ExcelExtractor(wb);
+			  extractor.setFormulasNotResults(true);
+			  extractor.setIncludeSheetNames(true);		  
+			  return extractor.getText();
+			  
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "";	
+		
 	}
+	
+	/**
+	 * Método para extrair texto xlsx
+	 * @param caminho
+	 * @return
+	 */
 
+public static String pegaTextoExcelXLSX(String caminho){
 
+		try{
+			OPCPackage oPCPackage = OPCPackage.open(caminho);
+			XSSFWorkbook xSSFWorkbook = new XSSFWorkbook(oPCPackage);
+			XSSFExcelExtractor documento = new XSSFExcelExtractor(xSSFWorkbook);			
+			return documento.getText();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "";
+	}
 }
